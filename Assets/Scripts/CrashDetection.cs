9f73a3b94e1d84a08b7681756c6b119585cfc230
Nate Honour <Honour067@morrisville.edu>
2022-03-01 22:38:55 -0500

    Triggering Particle Effects

--- Assets/Scripts/CrashDetection.cs
+++ Assets/Scripts/CrashDetection.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class CrashDetection : MonoBehaviour
 {
+   [SerializeField] ParticleSystem crashEffect;
    [SerializeField] float loadDelay = 1f;
     void OnTriggerEnter2D(Collider2D other) 
    {
        if(other.tag == "Ground")
     {
+       crashEffect.Play();
        Invoke("ReloadScene",loadDelay);
     }
    }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class FinishLine : MonoBehaviour
 {
  
+[SerializeField] ParticleSystem finsihEffect;
 [SerializeField] float loadDelay = 1f;
 void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Player")
         {
+            finsihEffect.Play();
             Invoke("ReloadScene", loadDelay); 
         }
     }
