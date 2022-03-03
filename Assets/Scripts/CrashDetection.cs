85524ecb7f244eed21fe53b972159348f81553ba
Nate Honour <Honour067@morrisville.edu>
2022-03-02 23:22:18 -0500

    Prevent Double Play

--- Assets/Scripts/CrashDetection.cs
+++ Assets/Scripts/CrashDetection.cs
@@ -8,10 +8,12 @@ public class CrashDetection : MonoBehaviour
    [SerializeField] AudioClip crashSFX;
    [SerializeField] ParticleSystem crashEffect;
    [SerializeField] float loadDelay = 1f;
+   bool hasCrashed = false;
     void OnTriggerEnter2D(Collider2D other) 
    {
-       if(other.tag == "Ground")
+       if(other.tag == "Ground" && !hasCrashed)
     {
+       hasCrashed = true;
        FindObjectOfType<PlayerController>().DisableControls();
        crashEffect.Play();
        GetComponent<AudioSource>().PlayOneShot(crashSFX);
